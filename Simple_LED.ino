010aadf1b50cdb8313c963f05096374482fc0aab
Edwinrose <edwinrose@gmail.com>
2018-06-27 00:16:38 +0800

    簡單LED閃爍

---
+++ Simple_LED.ino
@@ -0,0 +1,14 @@
+int ledPin = 2; //GPIO2 (D4)
+void setup() 
+{
+pinMode(ledPin, OUTPUT);
+}
+
+
+void loop()
+{
+digitalWrite(ledPin, HIGH); 
+delay(100); 
+digitalWrite(ledPin, LOW); 
+delay(100); 
+}
